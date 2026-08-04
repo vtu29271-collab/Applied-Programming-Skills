44d72923aba30e98950944a1f79244becde2653b
vtu29271-collab <vtu29271@veltech.edu.in>
2026-08-04 14:45:29 +0530

    Time: 2 ms (92.13%), Space: 48.1 MB (6.98%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int ip=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[ip++]=nums[i];
+            }
+        }  
+        while(ip<nums.length){
+            nums[ip]=0;
+            ip++;
+        }
+    }
+}
