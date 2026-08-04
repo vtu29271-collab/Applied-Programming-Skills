a2768c4a6972823c89c427a49c9f26cc4f3e1789
vtu29271-collab <vtu29271@veltech.edu.in>
2026-08-04 14:39:57 +0530

    Time: 0 ms (100%), Space: 48.1 MB (85.81%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        int target = 9;
+
+        System.out.println(s.search(nums, target));
+    }
+}
