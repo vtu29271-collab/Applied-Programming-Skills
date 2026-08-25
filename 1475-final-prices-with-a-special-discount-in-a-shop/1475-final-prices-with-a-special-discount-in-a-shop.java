264fbf6ac05777d248003e7c8a1fb073c9451650
vtu29271-collab <vtu29271@veltech.edu.in>
2026-08-25 14:09:32 +0530

    Time: 4 ms (50.23%), Space: 45.2 MB (74.67%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int[] result = prices.clone();
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < prices.length; i++) {
+            while (!stack.isEmpty() && prices[stack.peek()] >= prices[i]) {
+                int index = stack.pop();
+                result[index] = prices[index] - prices[i];
+            }
+            stack.push(i);
+        }
+        return result;
+    }
+}
