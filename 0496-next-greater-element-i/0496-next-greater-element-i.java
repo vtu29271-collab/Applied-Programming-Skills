ac4b0d81ffb4bed9a723437ccaf9603e23899167
vtu29271-collab <vtu29271@veltech.edu.in>
2026-09-15 14:16:30 +0530

    Time: 4 ms (73.57%), Space: 45.2 MB (49.59%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer,Integer>nextGreaterMap=new HashMap<>();
+        Stack<Integer>stack=new Stack<>();
+        for(int num:nums2){
+            while(!stack.isEmpty()&&stack.peek()<num){
+                nextGreaterMap.put(stack.pop(),num);
+            }
+            stack.push(num);
+        }
+        int[] ans=new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            ans[i]=nextGreaterMap.getOrDefault(nums1[i],-1);
+        }
+        return ans;
+    }
+}
